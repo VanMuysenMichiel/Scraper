29907f48ff954d885bef7c76783bec78c1fde26a
VanMuysenMichiel <91471320+VanMuysenMichiel@users.noreply.github.com>
2022-04-25 20:50:52 +0200

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -4,26 +4,30 @@ from time import time, sleep
 import logging 
 
 x = 0
+today = date.today()
 
 while True:
     r = requests.get("https://www.blockchain.com/btc/unconfirmed-transactions")
-    soup = BeautifulSoup(r.text, features="html.parser")
+    soup = BeautifulSoup(r.text)
     tags = soup.findAll('div', attrs={"class" : "sc-6nt7oh-0 PtIAf"})
     data = []
     for tag in tags:
       k = tag.text
-      data.append(k)
+      y= k.replace(" BTC", "")
+      data.append(y)
     hashes = data[0::4]
     times = data[1::4]
-    BTC = data[2::4]
+    BTC1 = data[2::4]
+    BTC = list(map(float, BTC1))
     USD = data[3::4]
     highest = max(BTC)
     index = BTC.index(highest)
-    output = [hashes[index],times[index],BTC[index],USD[index]]
+    output = [hashes[index],times[index],str(BTC[index])+" BTC",USD[index]]
     if x == 0:
       logging.basicConfig(filename='VanMuysenMichiel.log', format='%(message)s', filemode='w')
       logger=logging.getLogger()
     if x > 0:
+      print(output)
       with open('VanMuysenMichiel.log', 'a') as f:
         f.writelines('\n'.join(output))
         f.writelines('\n')
